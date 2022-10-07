26b4ca77313ee1a34915ba14e5afc9ea78a56baa
Ekemini <42721358+b4ssey@users.noreply.github.com>
2022-10-07 10:54:24 +0100

    first commit of 2nd api

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express'); 
+const app = express();             
+const port = 5001;                  
+
+let YOUR_WORST_FOOD = "peas"
+let YOUR_NAME = "ekemini"
+
+app.get('/', (req, res) => {       
+    res.send(`Welcome to ${YOUR_NAME}'s 2nd API`);     
+                                                     
+});
+
+app.get('/worstfood', (req, res) => {       
+    res.send(`My worst food is ${YOUR_WORST_FOOD}`);                                                       
+});
+
+app.get('/worstfoodsource', async (req, res) => {       
+    res.send(YOUR_WORST_FOOD);                                                       
+});
+
+app.listen(port, () => {          
+    console.log(`Now listening on port ${port}`); 
+});
